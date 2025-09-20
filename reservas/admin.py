7f21d4982738d4235e283fb1358aa0b2204606ba
Viviane Silva <viviane.leite.silva@gmail.com>
2025-09-20 18:57:50 -0300

    feat: ajuste Django Admin para permitir edição de reservas e filtros

--- reservas/admin.py
+++ reservas/admin.py
@@ -6,4 +6,6 @@ from .models import Reserva
 @admin.register(Reserva)
 class ReservaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'data_reserva', 'horario', 'criacao', 'periodo', 'estacao')
-
+    list_editable = ('estacao', 'data_reserva', 'periodo')  # Campos que podem ser editados diretamente na lista
+    search_fields = ('nome_usuario', 'estacao__nome')  
+    list_filter = ('data_reserva', 'periodo')
